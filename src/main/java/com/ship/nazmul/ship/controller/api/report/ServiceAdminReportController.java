4200f709276276e94aeba07d4f1102e790270d57
Nazmul Alam <nazmuldipu@gmail.com>
2019-05-19 14:52:44 +0600

    json exception handled 02

--- src/main/java/com/ship/nazmul/ship/controller/api/report/ServiceAdminReportController.java
+++ src/main/java/com/ship/nazmul/ship/controller/api/report/ServiceAdminReportController.java
@@ -3,6 +3,7 @@ package com.ship.nazmul.ship.controller.api.report;
 import com.ship.nazmul.ship.exceptions.forbidden.ForbiddenException;
 import com.ship.nazmul.ship.exceptions.notfound.NotFoundException;
 import com.ship.nazmul.ship.services.ReportService;
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -22,17 +23,17 @@ public class ServiceAdminReportController {
     }
 
     @GetMapping("")
-    private ResponseEntity getServiceAdminDashboardReport(@RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) throws ForbiddenException {
+    private ResponseEntity getServiceAdminDashboardReport(@RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) throws ForbiddenException, JSONException {
         return ResponseEntity.ok(this.reportService.getServiceAdminDashboardReport(date).toString());
     }
 
     @GetMapping("/shipSells/{shipId}")
-    private ResponseEntity getAdminBookingReportByHotelId(@PathVariable("shipId") Long shipId, @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) throws ForbiddenException, ParseException {
+    private ResponseEntity getAdminBookingReportByHotelId(@PathVariable("shipId") Long shipId, @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) throws ForbiddenException, ParseException, JSONException {
         return ResponseEntity.ok(this.reportService.getServiceAdminSellsReport(shipId, date));
     }
 
     @GetMapping("/shipReservation/{shipId}")
-    private ResponseEntity getAdminReservationReportByHotelId(@PathVariable("shipId") Long shipId, @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) throws NotFoundException, ForbiddenException, ParseException {
+    private ResponseEntity getAdminReservationReportByHotelId(@PathVariable("shipId") Long shipId, @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) throws JSONException, ForbiddenException{
         return ResponseEntity.ok(this.reportService.getServiceAdminReservationReport(shipId, date));
     }
 }
