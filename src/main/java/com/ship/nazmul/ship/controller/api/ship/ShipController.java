c4a500f3b91fb588fdfbe2e12dd0edc5724f25eb
Nazmul Alam <nazmuldipu@gmail.com>
2019-09-03 19:06:24 +0600

    user get ship done

--- src/main/java/com/ship/nazmul/ship/controller/api/ship/ShipController.java
+++ src/main/java/com/ship/nazmul/ship/controller/api/ship/ShipController.java
@@ -1,14 +1,12 @@
 package com.ship.nazmul.ship.controller.api.ship;
 
 import com.ship.nazmul.ship.entities.Ship;
+import com.ship.nazmul.ship.exceptions.notfound.NotFoundException;
 import com.ship.nazmul.ship.services.ShipService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/ships")
@@ -21,8 +19,13 @@ public class ShipController {
     }
 
     @GetMapping("")
-    public ResponseEntity<Page<Ship>> getAllHotels(@RequestParam(value = "query", required = false, defaultValue = "") String query,
+    public ResponseEntity<Page<Ship>> getAllShips(@RequestParam(value = "query", required = false, defaultValue = "") String query,
                                                    @RequestParam(value = "page", defaultValue = "0") Integer page) {
         return ResponseEntity.ok(this.shipService.searchShip(query, page));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ship> getShipWithId(@PathVariable("id") Long id) throws NotFoundException {
+        return ResponseEntity.ok(this.shipService.getOne(id));
     }
 }
