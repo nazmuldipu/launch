38b091cbe4c0bee16a1db22436d754dacebc4fb2
Nazmul Alam <nazmuldipu@gmail.com>
2019-11-16 11:40:37 +0600

    Debug point added 04

--- src/main/java/com/ship/nazmul/ship/services/impl/BookingServiceImpl.java
+++ src/main/java/com/ship/nazmul/ship/services/impl/BookingServiceImpl.java
@@ -360,6 +360,9 @@ public class BookingServiceImpl implements BookingService {
             SubBooking newSubBooking = new SubBooking();
             System.out.println("BB0 : " + new Date());
 
+            newSubBooking.setSeat(seat);
+            System.out.println("CB 04 : " + new Date());
+
             LocalDate ld = subBooking.getDate();
             newSubBooking.setDate(ld);
             System.out.println("BB1 :" + new Date());
@@ -372,8 +375,6 @@ public class BookingServiceImpl implements BookingService {
             newSubBooking.setCommission(commit);
             System.out.println("BB3 :" + new Date());
 
-            newSubBooking.setSeat(seat);
-            System.out.println("CB 04 : " + new Date());
             // 4) Calculate each subBooking and add to subBookingList
             System.out.println("CB 05 : " + new Date());
             newSubBooking = this.calculateSubBooking(newSubBooking);
